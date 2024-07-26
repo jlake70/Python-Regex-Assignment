3eba5ad0762243a0a88b8458a80100ea6254fdec
jlake70 <jlake7021@gmail.com>
2024-07-26 14:14:54 -0400

    Created a function for the for loop

--- 1. Python Regular Expressions Deep Dive.py
+++ 1. Python Regular Expressions Deep Dive.py
@@ -8,9 +8,10 @@ exclude_domains = "exclude.com"
 text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
 emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
 
+def excluded_emails():
+    for email in emails:
+        if not email.endswith(f"@exclude.com"):
+            exclude_emails.append(emails)
+    print(emails)
 
-for email in emails:
-    if not email.endswith(f"@exclude.com"):
-        exclude_emails.append(emails)
-
-print(emails)
+excluded_emails()
