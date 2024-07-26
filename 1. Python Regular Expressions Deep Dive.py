d31e3f1d28c0a076f02293d2dc41fd552f1fde7e
jlake70 <jlake7021@gmail.com>
2024-07-26 14:09:32 -0400

    Completed task 1 with a nested for loop

---
+++ 1. Python Regular Expressions Deep Dive.py
@@ -0,0 +1,16 @@
+
+
+import re
+exclude_emails = []
+exclude_domains = "exclude.com"
+
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
+
+
+for email in emails:
+    if not email.endswith(f"@exclude.com"):
+        exclude_emails.append(emails)
+
+print(emails)
